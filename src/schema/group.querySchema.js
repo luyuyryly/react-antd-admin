cb3bfad63af766f32610cbbf528c5bbe4bacfd5a
luyuluyu <luyuluyu@didichuxing.com>
2018-11-27 17:17:05 +0800

    分组列表

--- src/schema/group.querySchema.js
+++ src/schema/group.querySchema.js
@@ -2,10 +2,10 @@
 // schema的结构和含义参考下面的例子
 // 注意: 所有的key不能重复
 
-module.exports = [
-  /*{
+/*module.exports = [
+  /!*{
     key: 'group',   // 传递给后端的字段名
     title: '机器组',    // 前端显示的名称
     dataType: 'varchar',
-  },*/
-];
+  },*!/
+];*/
